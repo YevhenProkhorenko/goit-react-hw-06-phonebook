b91c286949e0f5362b722ca884697c987ea22288
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-10-28 23:32:48 +0300

    fixed non-serialized

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { persistContacts } from './reducers';
 export const store = configureStore({
   reducer: {
     contacts: persistContacts,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export const persistore = persistStore(store);
